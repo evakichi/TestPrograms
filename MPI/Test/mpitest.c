07c23432f281169dc8b9741f7c38b15491789e21
Makoto ISHIHARA <evakichi@be.wakwak.com>
2020-04-29 08:04:12 +0900

    add text for MPI/Test/test.c

--- MPI/Test/mpitest.c
+++ MPI/Test/mpitest.c
@@ -6,6 +6,6 @@ int main(int argc,char *argv[])
 	MPI_Init(&argc,&argv);
 	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 	MPI_Comm_size(MPI_COMM_WORLD,&size);
-	printf("%d/%d\n",rank,size);
+	printf("Hello I'm %d/%d\n",rank,size);
 	MPI_Finalize();
 }
